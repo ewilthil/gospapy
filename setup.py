1b18b56f1bb6da050a3a21eb202773ed8f6a05a2
Erik Wilthil <erik.wilthil@gmail.com>
2019-08-19 11:26:13 +0200

    Initial information on the package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='gospa_python',
+      version='0.1',
+      description='Python implementation of the generalized optimal subpattern
+        assignment (GOSPA).'
+      url='',
+      author='Erik Wilthil',
+      author_email='erik.wilthil@ntnu.no',
+      license='MIT',
+      packages=['gospa_python']
+      )
