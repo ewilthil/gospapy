492a44dc310a01fa04ca383abed1de0b3b700917
Erik Wilthil <erik.wilthil@gmail.com>
2019-08-20 09:19:21 +0200

    Rename number of tracks and target variables

--- gospapy/gospa.py
+++ gospapy/gospa.py
@@ -57,21 +57,19 @@ def calculate_gospa(targets, tracks, c, p, alpha=2,
       presentation, 2017. Available online: https://youtu.be/M79GTTytvCM
     """
     check_gospa_parameters(c, p, alpha)
-    dim_targets = len(targets)
-    dim_tracks = len(tracks)
+    num_targets = len(targets)
+    num_tracks = len(tracks)
     miss_cost = c**p/alpha
-    if dim_targets == 0: # All the tracks are false tracks
-        gospa_false = miss_cost*dim_tracks
-        assignment = dict()
-        return gospa_false**(1/p), assignment, 0, 0, gospa_false
-    elif dim_tracks == 0: # All the targets are missed
-        gospa_missed = miss_cost*dim_targets
-        assignment = dict()
-        return gospa_missed**(1/p), assignment, 0, gospa_missed, 0
+    if num_targets == 0: # All the tracks are false tracks
+        gospa_false = miss_cost*num_tracks
+        return gospa_false**(1/p), dict(), 0, 0, gospa_false
+    elif num_tracks == 0: # All the targets are missed
+        gospa_missed = miss_cost*num_targets
+        return gospa_missed**(1/p), dict(), 0, gospa_missed, 0
     else: # There are elements in both sets. Compute cost matrix
-        cost_matrix = np.zeros((dim_targets, dim_tracks))
-        for n_target in range(dim_targets):
-            for n_track in range(dim_tracks):
+        cost_matrix = np.zeros((num_targets, num_tracks))
+        for n_target in range(num_targets):
+            for n_track in range(num_tracks):
                 current_cost = assignment_cost_function(
                     targets[n_target], tracks[n_track])**p
                 cost_matrix[n_target,n_track] = np.min([
@@ -84,8 +82,8 @@ def calculate_gospa(targets, tracks, c, p, alpha=2,
                 gospa_localization += cost_matrix[target_idx, track_idx]
                 target_to_track_assigments[target_idx] = track_idx
         num_assignments = len(target_to_track_assigments)
-        num_missed = dim_targets - num_assignments
-        num_false = dim_tracks - num_assignments
+        num_missed = num_targets - num_assignments
+        num_false = num_tracks - num_assignments
         gospa_missed = miss_cost*num_missed
         gospa_false = miss_cost*num_false
         gospa = (gospa_localization + gospa_missed + gospa_false)**(1/p)
